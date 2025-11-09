9c2d8594d4e3d40513e76ea279de1365e2d1658c
grwna <13523035@std.stei.itb.ac.id>
2025-11-09 18:18:08 +0700

    init

---
+++ UnitTest.py
@@ -0,0 +1,19 @@
+class StorageEngine:
+    def __init__(self):
+        self.data = {}
+
+    def read_block():
+        
+        pass
+
+    def write_block():
+        pass
+
+    def delete_block():
+        pass
+
+    def set_index():
+        pass
+
+    def get_stats():
+        pass
